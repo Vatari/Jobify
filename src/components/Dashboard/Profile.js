f97f5edf520a2272c946f0cecfffef84b9c9d4bf
vatari <majestic13@gmail.com>
2023-02-26 10:08:20 +0200

    added profile page

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -1,5 +1,79 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+import { FormRow } from "..";
+import Wrapper from "./ProfileWrapper";
+
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    lastName: user?.lastName || "",
+    email: user?.email || "",
+    location: user?.location || "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      !userData.name ||
+      !userData.email ||
+      !userData.lastName ||
+      !userData.location
+    ) {
+      toast.error("Моля попълнете всички полета");
+      return;
+    }
+  };
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>Профил</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            labelText="Име"
+            name="name"
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="Фамилия"
+            name="lastName"
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="Град"
+            name="location"
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button className="btn btn-lock" type="submit" disabled={isLoading}>
+            {isLoading ? "Заявката се обработва..." : "Запиши промените"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
