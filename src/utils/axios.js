17cf19590d77afdaacd56bbf6904a9a3f259f9b8
vatari <majestic13@gmail.com>
2023-03-12 20:04:11 +0200

    moved baseURL to my server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customFetch = axios.create({
   //baseURL: "http://localhost:4000",
-  baseURL: "https://66c005c045ce.sn.mynetname.net:4000",
+  baseURL: "http://66c005c045ce.sn.mynetname.net:4000",
 });
 
 export default customFetch;
