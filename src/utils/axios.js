f9656a56230f44becdc61ec4f70a74be23b75741
vatari <majestic13@gmail.com>
2023-03-12 19:15:54 +0200

    moved baseURL to https on my server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "http://localhost:4000",
+  //baseURL: "http://localhost:4000",
+  baseURL: "https://66c005c045ce.sn.mynetname.net:4000",
 });
 
 export default customFetch;
