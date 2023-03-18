307ed92b1a7dafdbd18683bc3ecbd49030b968e8
vatari <majestic13@gmail.com>
2023-03-18 16:51:50 +0200

    added AreaChart logic

--- src/components/Dashboard/AreaChart.js
+++ src/components/Dashboard/AreaChart.js
@@ -1,5 +1,24 @@
-const AreaChart = () => {
-  return <div>AreaChart</div>;
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+const AreaChartComponent = (data) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
-export default AreaChart;
+export default AreaChartComponent;
