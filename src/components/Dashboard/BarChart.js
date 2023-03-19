acb1371a8c2eb785b43b7c8b30f19bb2ffa7ae6c
vatari <majestic13@gmail.com>
2023-03-19 19:24:33 +0200

    added bar chart

--- src/components/Dashboard/BarChart.js
+++ src/components/Dashboard/BarChart.js
@@ -1,5 +1,22 @@
-const BarChart = () => {
-  return <div>BarChart</div>;
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const BarChartComponent = ({ data }) => {
+  <ResponsiveContainer width="100%" height={300}>
+    <BarChart data={data} margin={{ top: 50 }} />
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="date" />
+    <YAxis allowDecimals={false} />
+    <Tooltip />
+    <Bar dataKey="count" fill="#3b82f6" barSize={75} />
+  </ResponsiveContainer>;
 };
 
 export default BarChart;
