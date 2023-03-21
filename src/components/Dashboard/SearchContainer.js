112d7f43316839cefc9819d51133438a5c508d69
vatari <majestic13@gmail.com>
2023-03-21 19:18:26 +0200

    added searchContainer logic

--- src/components/Dashboard/SearchContainer.js
+++ src/components/Dashboard/SearchContainer.js
@@ -1,5 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { FormRow, FormRowSelect } from ".";
+import Wrapper from "./SearchContainerWrapper";
+
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOPtions } =
+    useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+  const handleSearch = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 };
 
 export default SearchContainer;
