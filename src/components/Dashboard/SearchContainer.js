e7e9d43957cb3a83af31734218bea4049b3d1789
vatari <majestic13@gmail.com>
2023-03-21 19:33:37 +0200

    finished search container

--- src/components/Dashboard/SearchContainer.js
+++ src/components/Dashboard/SearchContainer.js
@@ -1,15 +1,58 @@
 import { useDispatch, useSelector } from "react-redux";
-import { FormRow, FormRowSelect } from ".";
+import { FormRow, FormRowSelect } from "..";
 import Wrapper from "./SearchContainerWrapper";
 
 const SearchContainer = () => {
   const { isLoading, search, searchStatus, searchType, sort, sortOPtions } =
     useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
   const dispatch = useDispatch();
   const handleSearch = (e) => {};
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>Търси</h4>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            labelText="Търси"
+            name="search"
+            value={handleSearch}
+          />
+          <FormRowSelect
+            labelText="Статус"
+            name="searchStatus"
+            value={searchStatus}
+            list={["all", ...statusOptions]}
+          />
+          <FormRowSelect
+            labelText="Тип"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={["all", ...jobTypeOptions]}
+          />
+          <FormRowSelect
+            labelText="Сортирай"
+            name="sort"
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOPtions}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            Изчисти полетата
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
