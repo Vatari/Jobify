cea7f1eb141bacfe898e8899aeb6dd17b95ff036
vatari <majestic13@gmail.com>
2023-03-26 08:26:40 +0300

    migrated server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "http://localhost:4000",
+  //baseURL: "http://localhost:4000",
+  baseURL: "http://b8e00a7b5ca8.sn.mynetname.net:3019/",
   // baseURL: "http://66c005c045ce.sn.mynetname.net:4000",
   // baseURL: "http://10.10.101.19:4000",
 });
