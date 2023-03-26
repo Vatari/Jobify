d6261d88878e6eed37c8f6153eea4af470b75c18
vatari <majestic13@gmail.com>
2023-03-26 22:15:12 +0300

    added firebase to project

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const customFetch = axios.create({
   //baseURL: "http://localhost:4000",
-  baseURL: "http://b8e00a7b5ca8.sn.mynetname.net:3019/",
-  // baseURL: "http://66c005c045ce.sn.mynetname.net:4000",
+  // baseURL: "http://b8e00a7b5ca8.sn.mynetname.net:3019/",
+  baseURL: "https://66c005c045ce.sn.mynetname.net",
   // baseURL: "http://10.10.101.19:4000",
 });
 
