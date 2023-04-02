22ad6a5551ed7e182366d390fc3d390b062053f2
vatari <majestic13@gmail.com>
2023-04-02 21:20:32 +0300

    implemented logic for pagination

--- src/components/Dashboard/PaginationContainer.js
+++ src/components/Dashboard/PaginationContainer.js
@@ -1,9 +1,22 @@
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import { useSelector, useDispatch } from "react-redux";
 
 import Wrapper from "./PaginationContainerWrapper";
 
 const PaginationContainer = () => {
-  return <h2>PaginationContainer</h2>;
+  const { numOfPages, page } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+  const nextPage = () => {};
+  const prevPage = () => {};
+  return (
+    <button className="prev-btn" onClick={prevPage}>
+      <HiChevronDoubleLeft />
+      Назад
+    </button>
+  );
 };
 
 export default PaginationContainer;
