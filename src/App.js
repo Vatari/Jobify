b3b19976c3f6c00d7f6bb5e668eb37c40749f53c
vatari <majestic13@gmail.com>
2023-04-05 19:08:32 +0300

    moved toast notifications to top-center for better view and UX

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      <ToastContainer />
+      <ToastContainer position="top-center" />
     </BrowserRouter>
   );
 }
