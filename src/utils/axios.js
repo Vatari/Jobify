204d939a0ba4f42950ce080f16e6184909db5f41
Petar Zhelev <majestic13@gmail.com>
2023-08-18 14:59:03 +0300

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customFetch = axios.create({
   //baseURL: "http://localhost:4000",
-  baseURL: "http://b8e00a7b5ca8.sn.mynetname.net:3019/",
+  baseURL: "jobify-server:3019/",
   // baseURL: "http://10.10.101.19:4000",
 });
 
